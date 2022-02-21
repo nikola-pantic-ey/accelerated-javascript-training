71e9b05034b969a7d5bf8bddd89fb5e3d8e24aca
Nikola Pantic <Nikola.Pantic@rs.ey.com>
2022-02-21 16:54:17 +0100

    errors handled

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ var button=document.querySelector('button')
 var city=document.querySelector('.search-result-city')
 var temperaturefield=document.querySelector('.search-result-temperature')
 var weatherfield=document.querySelector('.search-result-weather')
-var apikey='7c0249f471691f748a9766827eda65d5'
 var searchresultfield=document.querySelector('.search-result')
+
+var apikey='7c0249f471691f748a9766827eda65d5'
 var request= new XMLHttpRequest()
-
-
 
 
 function searchWeather(data){
@@ -20,7 +19,7 @@ weatherfield.textContent=data.weather[0].description
 }
 
 function fetchData(){
-
+if(input.value!==''){
     request.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=`+apikey)
 
     
@@ -30,13 +29,14 @@ function fetchData(){
            searchWeather(JSON.parse(request.responseText))
         }
 
-        else if(request.readyState===XMLHttpRequest.DONE && request.status!==200) alert(JSON.parse(request.responseText.message))
-        // else if(request.readyState===XMLHttpRequest.DONE) alert('Something went wrong.')
+        else if(request.readyState===XMLHttpRequest.DONE && request.status===404) alert(JSON.parse(request.responseText).message)
+        else if(request.readyState===XMLHttpRequest.DONE) alert('Something went wrong.')
         
     }
 
     request.send()
-    
+}
+else alert('Please enter city name')
 }
 
 
